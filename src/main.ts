c52d9ee26c07761aaadfc89dfdaa0be2627b489b
lss233 <8984680+lss233@users.noreply.github.com>
2023-05-10 21:29:30 +0800

    feat: add login page

--- src/main.ts
+++ src/main.ts
@@ -12,3 +12,26 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+
+const oldFetch = window.fetch
+window.fetch = async (input: RequestInfo | URL, init?: RequestInit) => {    
+    if(localStorage.getItem('token')) {
+        if(init == undefined) {
+            init = {}
+        }
+        if(init['headers'] == undefined) {
+            init['headers'] = {}
+        }
+        init['headers']['Authorization'] = `Bearer ${localStorage.getItem('token')}`
+    }
+
+    let response = await oldFetch(input, init)
+    if(router?.currentRoute?.value?.name != 'login') {
+        if(response.status == 401) {
+            router.push('/login')
+        }
+    }
+    
+    return response
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/LoginView.vue'),
+    },
+    {
       path: '/',
       component: AppLayout,
       children: [
